26791db9fc2521db823ac93ddb6183a5df4d95ba
Yader Hernandez <yader.panzer88@gmail.com>
2022-06-30 13:07:40 -0500

    implementaciones y permiso de internet, creacion de modelo de datos

--- app/src/main/java/com/example/examen_azteca/MainActivity.kt
+++ app/src/main/java/com/example/examen_azteca/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.examen_azteca
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.examen_azteca.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private  lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/com/example/examen_azteca/models/datosPrueba.kt
@@ -0,0 +1,43 @@
+package com.example.examen_azteca.models
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class datosPrueba(
+    @SerializedName("items")
+    @Expose
+    var items: List<DatosIntems>
+)
+
+data class DatosIntems(
+    @SerializedName("firebasescreen")
+    @Expose
+    var firebasescreen: String,
+    @SerializedName("firebase_tipo")
+    @Expose
+    var firebase_tipo: String,
+    @SerializedName("tituloselcolor")
+    @Expose
+    var tituloselcolor: String,
+    @SerializedName("tipo")
+    @Expose
+    var tipo: String,
+    @SerializedName("imagetv")
+    @Expose
+    var imagetv: String,
+    @SerializedName("private")
+    @Expose
+    var private: String,
+    @SerializedName("programa")
+    @Expose
+    var programa: String,
+    @SerializedName("imagen")
+    @Expose
+    var imagen: String,
+    @SerializedName("firebase_screen")
+    @Expose
+    var firebase_screen: String,
+    @SerializedName("titulounselcolor")
+    @Expose
+    var titulounselcolor: String,
+)
